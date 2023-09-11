8ed27d2806ff2b1b60a488a17434f9f7afd5b92d
nedmac99 <109418428+nedmac99@users.noreply.github.com>
2023-09-10 21:54:22 -0400

    adding in TV and TvController classes
    I added the TV class and TvController class

---
+++ TvRemote/src/TvPractice/TV.java
@@ -0,0 +1,53 @@
+package TvPractice;
+
+	import java.util.Scanner;
+
+	public class TV {
+
+		boolean isOn = false;
+		int channel = 1;
+		int volume = 0;
+
+		Scanner input = new Scanner(System.in);
+
+		public void turnOn() {
+			isOn = true;
+		}
+
+		public void turnOff() {
+			isOn = false;
+		}
+
+		public void setChannel(int newChannel) {
+			if (isOn && newChannel >= 1 && newChannel <= 120)
+				channel = newChannel;
+		}
+
+		public void setVolume(int newVolume) {
+
+			if (isOn && newVolume >= 1 && newVolume <= 7)
+				volume = newVolume;
+		}
+
+		public void channelUp() {
+			if (isOn && channel < 120)
+				channel++;
+		}
+
+		public void channelDown() {
+			if (isOn && channel > 1)
+				channel--;
+		}
+
+		public void volumeUp() {
+			if (isOn && volume < 7)
+				volume++;
+		}
+
+		public void volumeDown() {
+			if (isOn && volume > 1)
+				volume--;
+		}
+
+	}
+
